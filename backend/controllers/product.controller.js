34907fbec22acabdf7f86dc8943e32fb041603af
shivam20234158 <149950525+shivam20234158@users.noreply.github.com>
2026-07-20 22:09:21 +0530

    product controller completed

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -71,3 +71,90 @@ export const createProduct = async (req, res) => {
 		res.status(500).json({ message: "Server error", error: error.message });
 	}
 };
+export const deleteProduct = async (req, res) => {
+	try {
+        //id is used in route delete
+		const product = await Product.findById(req.params.id);
+
+		if (!product) {
+			return res.status(404).json({ message: "Product not found" });
+		}
+
+		if (product.image) {
+			const publicId = product.image.split("/").pop().split(".")[0];//give id of image
+			try {
+                //produts folder stored it
+				await cloudinary.uploader.destroy(`products/${publicId}`);
+				console.log("deleted image from cloduinary");
+			} catch (error) {
+				console.log("error deleting image from cloduinary", error);
+			}
+		}
+
+		await Product.findByIdAndDelete(req.params.id);
+
+		res.json({ message: "Product deleted successfully" });
+	} catch (error) {
+		console.log("Error in deleteProduct controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+}; 
+export const getRecommendedProducts = async (req, res) => {
+	try {
+		const products = await Product.aggregate([
+			{
+				$sample: { size: 3 },
+			},
+			{
+				$project: {
+					_id: 1,
+					name: 1,
+					description: 1,
+					image: 1,
+					price: 1,
+				},
+			},
+		]);
+
+		res.json(products);
+	} catch (error) {
+		console.log("Error in getRecommendedProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
+export const getProductsByCategory = async (req, res) => {
+	const { category } = req.params;
+	try {
+		const products = await Product.find({ category });
+		res.json({ products });
+	} catch (error) {
+		console.log("Error in getProductsByCategory controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
+export const toggleFeaturedProduct = async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
+		if (product) {
+			product.isFeatured = !product.isFeatured;
+			const updatedProduct = await product.save();
+			await updateFeaturedProductsCache();
+			res.json(updatedProduct);
+		} else {
+			res.status(404).json({ message: "Product not found" });
+		}
+	} catch (error) {
+		console.log("Error in toggleFeaturedProduct controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
+async function updateFeaturedProductsCache() {
+	try {
+		// The lean() method  is used to return plain JavaScript objects instead of full Mongoose documents. This can significantly improve performance
+
+		const featuredProducts = await Product.find({ isFeatured: true }).lean();
+		await redis.set("featured_products", JSON.stringify(featuredProducts));
+	} catch (error) {
+		console.log("error in update cache function");
+	}
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getAllProducts,getFeaturedProducts,createProduct} from "../controllers/product.controller.js"; 
+import {getAllProducts,getFeaturedProducts,createProduct,deleteProduct,getRecommendedProducts,getProductsByCategory,toggleFeaturedProduct} from "../controllers/product.controller.js"; 
 import { protectRoute,adminRoute } from "../middleware/auth.middleware.js";
 
 const router=express.Router();
@@ -7,4 +7,9 @@ const router=express.Router();
 router.get("/",protectRoute,adminRoute,getAllProducts);
 router.get("/featured",getFeaturedProducts);
 router.post("/",protectRoute,adminRoute,createProduct);
-export default router
+router.delete("/:id",protectRoute,adminRoute,deleteProduct);
+router.get("/recommendations",getRecommendedProducts);
+router.get("/category/:category",getProductsByCategory);
+router.patch("/:id",protectRoute,adminRoute,toggleFeaturedProduct);
+
+export default router;
