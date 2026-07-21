ae5f94017d8a9ce3ed9bae82be7ebd5d40ecfb72
raunak singh <raunaksingh@raunaks-MacBook-Air.local>
2026-07-21 22:44:24 +0530

    payment route added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 //alllow to read content of .env file and add all variable to process.env
 dotenv.config();
 
@@ -14,6 +16,8 @@ const PORT=process.env.PORT || 5001;
 app.use("/api/auth",authRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/cart",cartRoutes);
+app.use("/api/coupons",couponRoutes);
+app.use("/api/payments",paymentRoutes);
 //our server is listening on this port
 connectDB();
 app.listen(PORT,()=>{
