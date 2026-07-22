fedb2e53b384a57ae0dc22aef25aa7338bc13f6f
shivam20234158 <149950525+shivam20234158@users.noreply.github.com>
2026-07-22 22:15:31 +0530

    product store created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import { Navigate,Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import CategoryPage from "./pages/CategoryPage";
