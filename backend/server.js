6c3f0b53829f731b69fa683135240594e25b1995
shivam20234158 <149950525+shivam20234158@users.noreply.github.com>
2026-07-24 19:15:33 +0530

    final changes

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 //routes
-import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
@@ -11,14 +12,24 @@ import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
 import aiRoutes from "./routes/ai.route.js";
+
+
+import { connectDB } from "./lib/db.js";
+
 //alllow to read content of .env file and add all variable to process.env
 dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT || 5001;
 
+const __dirname=path.resolve();
+
+
+//middlewares
 app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
+
+app.use("/api/ai", aiRoutes);
 
 app.use("/api/auth",authRoutes);
 app.use("/api/products",productRoutes);
@@ -26,7 +37,15 @@ app.use("/api/cart",cartRoutes);
 app.use("/api/coupons",couponRoutes);
 app.use("/api/payments",paymentRoutes);
 app.use("/api/analytics",analyticsRoutes);
-app.use("/api/ai", aiRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+	app.get(/.*/, (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 //our server is listening on this port
 connectDB();
 app.listen(PORT,()=>{
